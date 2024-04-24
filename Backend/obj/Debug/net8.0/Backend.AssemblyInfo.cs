b1a46d2a55e6c4027d2c52998e80d0085b68a007
Amr <amrmohamed.amaa@gmail.com>
2024-04-24 18:01:26 +0200

    editing the .net unwanted directories

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76afa6bcc2852aa3c5f7044e93d521ced042a6b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68ed0ca4b91d916919dcd192f277594ae23820f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
