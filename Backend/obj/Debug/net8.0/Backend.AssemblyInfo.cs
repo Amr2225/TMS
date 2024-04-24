732b621b7277bbdf15b7c1e26eb321a3b400f76a
Amr <amrmohamed.amaa@gmail.com>
2024-04-24 17:46:18 +0200

    test

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15a32cc7c689adc7a8af6f1f60dd820d3c876459")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8044d3050dc775051e9da99a3d53158e71d634f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
