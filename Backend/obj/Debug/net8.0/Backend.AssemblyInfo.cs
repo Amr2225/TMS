6ea615ec3a8f04dffb4c18dddef13fb6b5bb9a75
Amr <amrmohamed.amaa@gmail.com>
2024-05-03 20:43:39 +0300

    deleted the migrations file

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3c78b2e1eb054bc0df14ed9774878237dc1da28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+470995a664d92c6347a6bf099b7437c679967b92")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
